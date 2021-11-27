cf627697ea7ad9a819e7e828b2d7bad03c1df413
Microlith57 <microlith57@gmail.com>
2021-11-27 22:26:19 +1300

    project setup
    Signed-off-by: Microlith57 <microlith57@gmail.com>

---
+++ GatekeepHelper.cs
@@ -0,0 +1,31 @@
+﻿using Monocle;
+using System;
+
+namespace Celeste.Mod.GatekeepHelper
+{
+  public class GatekeepHelper : EverestModule
+  {
+    public static GatekeepHelper Instance;
+
+    public GatekeepHelper()
+    {
+      Instance = this;
+    }
+
+    public override void Load()
+    {
+    }
+
+    public override void Initialize()
+    {
+    }
+
+    public override void LoadContent(bool firstLoad)
+    {
+    }
+
+    public override void Unload()
+    {
+    }
+  }
+}
